cc423c070e9e73c97396988158ed6e57cae4666e
adianbu <theadithyaanbu@gmail.com>
2025-09-24 23:59:09 +0530

    feat(user): add sorting support to user list endpoint
    Allow sorting users by name or email via query parameter. Defaults to name if invalid sort field is provided.

--- src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -5,12 +5,12 @@ import com.codewithmosh.store.entities.User;
 import com.codewithmosh.store.mappers.UserMapper;
 import com.codewithmosh.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
 
 @RestController
 @AllArgsConstructor
@@ -20,8 +20,11 @@ public class UserController {
     private final UserMapper userMapper;
 
     @GetMapping
-    public Iterable<UserDto> getAllUser(){
-        return userRepository.findAll()
+    public Iterable<UserDto> getAllUser(@RequestParam(defaultValue = "", required = false) String sort){
+        if(!Set.of("name","email").contains(sort))
+            sort="name";
+
+        return userRepository.findAll(Sort.by(sort))
                 .stream()
                 .map(userMapper::toDto)
                 .toList();
